ea4db9db6e494dc1ec17b1acc20fadc9e8e37888
Iori Yanokura <ab.ioryz@gmail.com>
2025-04-07 17:07:34 +0900

    Revert "Only install"
    This reverts commit 29bdd9ad4c88827bd9ff6023469cff76202e35d6.

--- bin/install_formamotus.py
+++ bin/install_formamotus.py
@@ -10,4 +10,5 @@ base_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
 zip_path = os.path.join(base_dir, "formamotus.zip")
 subprocess.run(["zip", "-r", zip_path, "formamotus"], check=True)
 bpy.ops.preferences.addon_install(filepath=zip_path)
+bpy.ops.preferences.addon_enable(module="formamotus")
 bpy.ops.wm.save_userpref()
